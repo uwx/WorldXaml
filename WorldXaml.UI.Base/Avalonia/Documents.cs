f3c1ab22ce1205fec3fc653637b2034fc8b92e1d
uwx <maxine@cybergal.com>
2026-05-26 19:48:00 +0200

    remove empty files

--- WorldXaml.UI.Base/Avalonia/Documents.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using Avalonia.LogicalTree;
-using Avalonia.Metadata;
-using WorldXaml.UI.Base;
-
-// ReSharper disable once CheckNamespace
--- WorldXaml.UI.Yoga/InlineCollection.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Collections;
-using Avalonia.LogicalTree;
-using Avalonia.Metadata;
-using WorldXaml.UI.Base;
-using WorldXaml.UI.Yoga;
