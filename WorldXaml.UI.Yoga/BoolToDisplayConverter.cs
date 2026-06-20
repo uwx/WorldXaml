19b71ab3d5d986043a4e39be72955365917e9c5c
uwx <maxine@cybergal.com>
2026-06-20 19:51:19 +0200

    add BoolToDisplayConverter

---
+++ WorldXaml.UI.Yoga/BoolToDisplayConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using WorldXaml.UI.Base;
+
+namespace WorldXaml.UI.Yoga;
+
+public class BoolToDisplayConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return b ? YgDisplay.Flex : YgDisplay.None;
+        }
+
+        return YgDisplay.None;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is YgDisplay d)
+        {
+            return d == YgDisplay.Flex;
+        }
+        return false;
+    }
+}
